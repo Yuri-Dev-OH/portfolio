3580144abc1b8248b97ad8406859eaf78f2d08ca
Yuri Gonçalves <yurigoncalvesds@gmail.com>
2026-03-09 18:41:00 -0300

    Delete portfolio_2-main directory

--- portfolio_2-main/js/script.js
+++
@@ -1,7 +0,0 @@
-function toggleCard(element) {
-    const todosOsCards = document.querySelectorAll('.card-tech');
-    todosOsCards.forEach(card => {
-        if (card !== element) card.classList.remove('active');
-    });
-    element.classList.toggle('active');
-}
